32bb17a159d3e91fac91c8292223b38247b6a437
Daria <DRomanova69@gmail.com>
2020-03-21 15:27:11 +0300

    first commit

---
+++ game.py
@@ -0,0 +1,18 @@
+import numpy as np
+count = 0   
+begin = 1
+end = 100                         # счетчик попыток
+number = np.random.randint(begin, end)    # загадали случайное число
+predict = 0 # предполагаемое число
+while True:                        # бесконечный цикл
+         
+    count += 1    # плюсуем попытку
+    predict = (begin + end) // 2
+    if number == predict: 
+      break    # выход из цикла, если угадали
+    elif number > predict: 
+      begin = predict
+    elif number < predict:  
+      end = predict
+        
+print (f"Вы угадали число {number} за {count} попыток.")
